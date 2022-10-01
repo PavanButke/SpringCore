ff184a081476f7a97c4d22cf87f11ba68c13514e
PavanButke <butkepavan55@gmail.com>
2022-10-01 15:53:54 +0530

    Accessing More than One Bean using Map , Set and Iterator

--- src/main/java/com/dap/practice/App.java
+++ src/main/java/com/dap/practice/App.java
@@ -3,6 +3,7 @@ package com.dap.practice;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.*;
 /**
  * Hello world!
  *
@@ -15,12 +16,26 @@ public class App
     	AbstractApplicationContext iocContainer = new ClassPathXmlApplicationContext("beans.xml");
     	//IoC container
     	
-    	//get the parent bean calling over iocContainer
-    	Item item= (Item)iocContainer.getBean("Item");
-    	System.out.println("Item bean "+ item);
+//    	//get the parent bean calling over iocContainer
+//    	Item item= (Item)iocContainer.getBean("Item");
+//    	System.out.println("Item bean "+ item);
+//    	
+//    	//get the child bean calling over iocContainer
+//    	Order order= (Order)iocContainer.getBean("Order");
+//    	System.out.println("Order bean "+ order);
     	
-    	//get the child bean calling over iocContainer
-    	Order order= (Order)iocContainer.getBean("Order");
-    	System.out.println("Order bean "+ order);
+    	Map<String , Order> mapOfOrders = iocContainer.getBeansOfType(com.dap.practice.Order.class);
+    	Set<String> set = mapOfOrders.keySet();
+    	Iterator<String> itr = set.iterator();
+    	
+    	double totalPrice=0.0;
+    	while(itr.hasNext()) {
+    		String beanId = itr.next();
+    		Order order = mapOfOrders.get(beanId);
+    		
+    		totalPrice = totalPrice + order.getItem().getPrice();
+    	}
+    	
+    	System.out.println("Total Price of Order "+ totalPrice);
     }
 }
