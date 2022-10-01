ae2f034b5c95100f4560f329e3919ca87456079e
PavanButke <butkepavan55@gmail.com>
2022-10-01 20:23:09 +0530

    lazy init

--- src/main/java/com/dap/practice/App.java
+++ src/main/java/com/dap/practice/App.java
@@ -20,22 +20,22 @@ public class App
 //    	Item item= (Item)iocContainer.getBean("Item");
 //    	System.out.println("Item bean "+ item);
 //    	
-//    	//get the child bean calling over iocContainer
-//    	Order order= (Order)iocContainer.getBean("Order");
-//    	System.out.println("Order bean "+ order);
+    	//get the child bean calling over iocContainer
+    	Order order= (Order)iocContainer.getBean("Order");
+    	System.out.println("Order bean "+ order);
     	
-    	Map<String , Order> mapOfOrders = iocContainer.getBeansOfType(com.dap.practice.Order.class);
-    	Set<String> set = mapOfOrders.keySet();
-    	Iterator<String> itr = set.iterator();
-    	
-    	double totalPrice=0.0;
-    	while(itr.hasNext()) {
-    		String beanId = itr.next();
-    		Order order = mapOfOrders.get(beanId);
-    		
-    		totalPrice = totalPrice + order.getItem().getPrice();
-    	}
-    	
-    	System.out.println("Total Price of Order "+ totalPrice);
+//    	Map<String , Order> mapOfOrders = iocContainer.getBeansOfType(com.dap.practice.Order.class);
+//    	Set<String> set = mapOfOrders.keySet();
+//    	Iterator<String> itr = set.iterator();
+//    	
+//    	double totalPrice=0.0;
+//    	while(itr.hasNext()) {
+//    		String beanId = itr.next();
+//    		Order order = mapOfOrders.get(beanId);
+//    		
+//    		totalPrice = totalPrice + order.getItem().getPrice();
+//    	}
+//    	
+//    	System.out.println("Total Price of Order "+ totalPrice);
     }
 }
